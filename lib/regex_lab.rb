f89a4c1d7ff1d678bc9749996fea93e9a74c8453
Harry Bloch <harryfbloch@gmail.com>
2018-09-25 19:51:15 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,21 +1,34 @@
 def starts_with_a_vowel?(word)
-  letter = word.scan(/\A/)
-  vowels = "aeiou".split()
-  vowels.find {|vowel| vowel == letter}
+  letter = word.scan(/\b[aeiouAEIOU]/)
+  puts letter
+   if letter.length == 1
+      return true
+   else 
+      return false 
+   end
 end
+starts_with_a_vowel?("arry")
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  retArray = text.scan(/\b[un]{2}\w+ing/i)
 end
 
 def words_five_letters_long(text)
-
+  retArray = text.scan(/\b[a-z]{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if text.match(/^[A-Z].+\W$/)
+    return true
+  else
+    return false
+  end
 end
 
 def valid_phone_number?(phone)
-
+if phone.match (/.?\d{3}.?\d{3}.?\d{4}\b/)
+  return true
+else
+  return false
+end
 end
