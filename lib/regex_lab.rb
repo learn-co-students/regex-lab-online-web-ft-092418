accc45d618239814a7bf6da841bfb9cb58e7e6bd
Doug Fitzgerald <dfitzge1@conncoll.edu>
2018-09-25 15:11:12 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,22 @@
+require "pry"
 def starts_with_a_vowel?(word)
-
+  #binding.pry
+  #word[0].match(/[aeiouAEIOU]/) == nil ? false : true 
+  word.match(/^[aeiouAEIOU]/) == nil ? false : true 
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\bun\S*ing/) 
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text.match(/^[A-Z].*[.?,]$/) == nil ? false : true 
 end
 
 def valid_phone_number?(phone)
-
+  phone.match(/\(?\d{3}\)?[\s\.\-]?\d{3}[\s\.\-]?\d{4}/) == nil ? false : true 
 end
