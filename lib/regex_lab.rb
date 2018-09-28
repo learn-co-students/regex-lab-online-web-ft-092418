ddf2623c4a01541e394ef6646159835ae9e9db86
larryvsdata <ermanbekaroglu@gmail.com>
2018-09-28 22:32:03 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,26 @@
 def starts_with_a_vowel?(word)
-
+result=word.match(/^[aeiouAEIOU]\w*/)
+result == nil ?  false : true
 end
 
-def words_starting_with_un_and_ending_with_ing(text)
 
+def words_starting_with_un_and_ending_with_ing(text)
+result=text.scan(/un\w+ing/)
+result
 end
 
 def words_five_letters_long(text)
-
+result=text.scan(/\b\w{5}\b/)
+result
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+result=text.match(/^[A-Z].+\W$/)
+result == nil ?  false : true
 end
 
 def valid_phone_number?(phone)
+result=phone.match(/.?\d{3}.?\d{3}.?\d{4}\b/)
+result == nil ?  false : true
 
 end
